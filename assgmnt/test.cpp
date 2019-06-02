61509930e25da26e82fd34a04b7f5ebc2354a5ae
saurav-chandra <ssaurav333@gmail.com>
2019-06-02 11:50:31 +0530

    update day and test.cpp

---
+++ assgmnt/test.cpp
@@ -0,0 +1,22 @@
+int cost[10][10],i,j,k,n,stk[10],top,v,visit[10],visited[10];
+int main()
+{
+    int m;
+    //clrscr();
+    cout <<"Enter no of vertices:";
+    cin >> n;
+    cout <<"Enter no of edges:";
+    cin >> m;
+    cout <<"\nEDGES \n";
+    for(k=1; k<=m; k++)
+    {
+        cin >>i>>j;
+        cost[i][j]=1;
+    }
+    cout <<"Enter initial vertex to traverse from:";
+    cin >>v;
+    cout <<"DFS ORDER OF VISITED VERTICES:";
+    cout << v <<" ";
+    visited[v]=1;
+    k=1;
+ 
